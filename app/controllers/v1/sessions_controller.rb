7b6885d0ca4b0e0f440edf82d6059faa3af20ef8
github username <rdg.brg@gmail.com>
2020-09-12 13:29:13 -0400

    SessionsController create action

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -1,7 +1,14 @@
 class V1::SessionsController < ApplicationController
 
   def create
+    user = User.find_by(email: params[:email])
 
+    if user && user.valid_password?(params[:password])
+      render json: user, status: :created
+      # render json: user.as_json(only: [:id, :email]), status: :created
+    else
+      heade(:unauthorized)
+    end
   end
 
   def destroy
